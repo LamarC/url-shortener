02611bbf444a89c8baa8e13ad70ed9e78af5746b
camkirk <camden.kirkland@gmail.com>
2019-02-22 11:57:56 -0600

    setup npm express and serve home page

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const PORT = process.env.PORT||3000;
+
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req, res) => {
-    res.send("Shorter url goes here");
+    res.send("index.html");
 });
 
 app.listen(PORT, () => {
